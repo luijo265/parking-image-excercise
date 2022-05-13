a58cfe074cd6851ee6f4cbeb486169c3655edefd
Luis briceño <ljbricen@bancolombia.com.co>
2022-05-12 22:55:02 -0500

    init parking image project

---
+++ src/main/java/com/parking/images/ParkingImagesApplication.java
@@ -0,0 +1,13 @@
+package com.parking.images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingImagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parking/images/ParkingImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
